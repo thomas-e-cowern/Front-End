b31fd678a824be4a247431cf1c36977740dc38b5
Thomas E Cowern <thomas.e.cowern@gmail.com>
2017-09-13 14:24:54 -0400

    Christmas App Initial commit
    Christmas App Initial commit

---
+++ ChristmasApp/controller.js
@@ -0,0 +1,15 @@
+var app = angular.module("Santa", []);
+
+app.controller("AppController", appCtr) {
+
+
+
+function setSanta($scope) {
+    
+    var main = this;
+    
+    this.message = "Successfully connected to controller"
+    
+    }
+    
+}
