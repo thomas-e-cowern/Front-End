8a8c9eddb377324f101a22af3786464391d480da
Thomas E Cowern <thomas.e.cowern@gmail.com>
2017-09-14 10:34:18 -0400

    Updated add name functionality

--- ChristmasApp/controller.js
+++ ChristmasApp/controller.js
@@ -1,15 +1,36 @@
 var app = angular.module("Santa", []);
 
-app.controller("AppController", appCtr) {
+app.controller("AppController", appCtrl);
 
+function appCtrl() {
 
-
-function setSanta($scope) {
-    
     var main = this;
     
-    this.message = "Successfully connected to controller"
+    main.nameList = [];
     
+    var hidden = false
+
+    main.message = "Successfully connected to controller";
+
+    main.addName = function () {
+
+        var newName = document.getElementById("name").value;
+        
+        console.log("newName: ", newName);
+        
+        if (!newName) {
+            
+            document.getElementById("name").placeholder = "Please type a name first!"
+            
+            return;
+            
+        }
+        
+        main.nameList.push(newName);
+        
+        document.getElementById("name").value = '';
+        
+        console.log(main.nameList);
     }
-    
+
 }
